2f70c64e6353fa5877a0768733ff964db0be2820
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-11-30 07:26:22 +0000

    Add BoricaApi. (Fork from Mirovit/BorikaApi)

---
+++ src/Vankosoft/BoricaApi/Exceptions/InvalidParameterException.php
@@ -0,0 +1,6 @@
+<?php namespace Vankosoft\BoricaApi\Exceptions;
+
+
+class InvalidParameterException extends \InvalidArgumentException
+{
+}
---
+++ src/Vankosoft/BoricaApi/README.md
@@ -0,0 +1,2 @@
+# BORICA API forked from https://github.com/mirovit/borica-api because is not mainened
+
